889e89939ea9a0a47269d9f7c63b68956e09e399
Logan Wohlers <lawohlers@gmail.com>
2019-02-14 23:30:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,8 +40,8 @@ def apply_clearance(cart)
   p cart
   cart.map do |k, v|
     if cart[k][:clearance]==true
-      puts "TEST"
-      #cart[k][:price] *= 0.8
+      #puts "TEST"
+      cart[k][:price] *= 0.8
     else
       v
     end
