1f3a70b4c9dcc9071940979feed826fc9e94b589
Logan Wohlers <lawohlers@gmail.com>
2019-02-15 00:59:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -58,7 +58,11 @@ def checkout(cart, coupons)
   p newcart
   total=0.0
   newcart.each do |k, v|
+    if (k.include?("COUPON")
+      total+=v[:price]
+    else
     total += v[:price]*v[:count]
+  end
   end
   total
 end
