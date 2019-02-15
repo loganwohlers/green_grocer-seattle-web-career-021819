51630c503bd6ae545307c267a553d04d10fcaf0a
Logan Wohlers <lawohlers@gmail.com>
2019-02-15 01:01:48 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,71 @@
 def consolidate_cart(cart)
-  # code here
+  newcart={}
+  cart.each do |hashcart|
+    hashcart.each do |k,v|
+      if(!newcart.has_key?(k))
+        newcart[k]={}
+        v.each do |k2, v2|
+          newcart[k][k2]=v2
+        end
+        newcart[k][:count]=1
+      else
+        newcart[k][:count]+=1
+      end
+    end
+  end
+  newcart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  newcart= {}
+  coupons.each do |save|
+    word=save[:item]
+    #check=false
+    if cart.has_key?(word)
+      if cart[word][:count]-save[:num]>=0
+        cart[word][:count]-=save[:num]
+      end
+      newword="#{word} W/COUPON"
+      if !newcart.has_key?(newword)
+        newcart[newword]={price: save[:cost],
+        clearance: cart[word][:clearance], count: 1}
+      else
+          newcart[newword][:count]+=1
+      end
+    end
+  end
+  newcart.merge(cart)
 end
+  
 
 def apply_clearance(cart)
-  # code here
+  #sick example of map on a hash
+  a=cart.map do |k, v|
+    if v[:clearance]
+      num=(v[:price]* 0.8).round(3)
+      v[:price] =num
+    end
+    [k,v]
+  end.to_h
 end
 
 def checkout(cart, coupons)
-  # code here
+  p coupons
+    puts ""
+  p cart
+    puts ""
+  newcart=apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  p newcart
+  total=0.0
+  newcart.each do |k, v|
+    if (k.include?("COUPON"))
+      total+=v[:price]
+    else
+    total += v[:price]*v[:count]
+    end
+  end
+  if total>100
+    total*=0.9
+  end
+  total
 end
